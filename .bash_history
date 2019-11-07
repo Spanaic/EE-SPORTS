3167c32fc33e9026fd8da160dc99239d200eeb98
Spanaic <spanaic@gmail.com>
2019-11-07 21:38:26 +0900

    docker環境構築完了

---
+++ .bash_history
@@ -0,0 +1,9 @@
+ls
+ps aux | grep puma
+rails s -p 3001 -b 0.0.0.0
+rails tmp:clear
+rails s -p 3001 -b 0.0.0.0
+mv /myapi/tmp/pids/server.pid ~/
+rails s -p 3001 -b 0.0.0.0
+pwd
+ls
--- db/schema.rb
+++ db/schema.rb
@@ -12,28 +12,28 @@
 
 ActiveRecord::Schema.define(version: 2019_11_04_102503) do
 
-  create_table "favorites", force: :cascade do |t|
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "hashtags", force: :cascade do |t|
+  create_table "hashtags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "hashname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["hashname"], name: "index_hashtags_on_hashname", unique: true
   end
 
-  create_table "hashtags_post_images", id: false, force: :cascade do |t|
-    t.integer "post_image_id"
-    t.integer "hashtag_id"
+  create_table "hashtags_post_images", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_image_id"
+    t.bigint "hashtag_id"
     t.index ["hashtag_id"], name: "index_hashtags_post_images_on_hashtag_id"
     t.index ["post_image_id"], name: "index_hashtags_post_images_on_post_image_id"
   end
 
-  create_table "notifications", force: :cascade do |t|
+  create_table "notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "visitor_id", null: false
     t.integer "visited_id", null: false
     t.integer "post_image_id"
@@ -45,10 +45,10 @@ ActiveRecord::Schema.define(version: 2019_11_04_102503) do
     t.index ["post_comment_id"], name: "index_notifications_on_post_comment_id"
     t.index ["post_image_id"], name: "index_notifications_on_post_image_id"
     t.index ["visited_id"], name: "index_notifications_on_visited_id"
-    t.index [nil], name: "index_notifications_on_visiter_id"
+    t.index ["visitor_id"], name: "index_notifications_on_visitor_id"
   end
 
-  create_table "post_comments", force: :cascade do |t|
+  create_table "post_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
     t.integer "post_image_id"
     t.integer "user_id"
@@ -56,16 +56,16 @@ ActiveRecord::Schema.define(version: 2019_11_04_102503) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "post_comments_users", id: false, force: :cascade do |t|
-    t.integer "post_comment_id"
-    t.integer "user_id"
+  create_table "post_comments_users", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_comment_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_comment_id"], name: "index_post_comments_users_on_post_comment_id"
     t.index ["user_id"], name: "index_post_comments_users_on_user_id"
   end
 
-  create_table "post_images", force: :cascade do |t|
+  create_table "post_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.string "post_image_id"
     t.text "caption"
@@ -75,14 +75,14 @@ ActiveRecord::Schema.define(version: 2019_11_04_102503) do
     t.string "image_name"
   end
 
-  create_table "relationships", force: :cascade do |t|
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "following_id"
     t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -101,7 +101,12 @@ ActiveRecord::Schema.define(version: 2019_11_04_102503) do
     t.string "uid"
     t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["profile_name"], name: "index_users_on_profile_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "hashtags_post_images", "hashtags"
+  add_foreign_key "hashtags_post_images", "post_images"
+  add_foreign_key "post_comments_users", "post_comments"
+  add_foreign_key "post_comments_users", "users"
 end
